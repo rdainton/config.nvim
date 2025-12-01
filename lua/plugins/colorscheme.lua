7ee3b7f20fe5c62218c44c136ba469c41b55b8ce
Ryan Dainton <ryandainton@live.com>
2025-12-01 15:30:53 +0000

    feat: adds colorscheme toggling

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,5 +4,9 @@ return {
   lazy = false,
   config = function()
     require('leather').setup {}
+
+    vim.keymap.set('n', '<leader>tt', function()
+      require('leather').toggle_theme()
+    end, { desc = '[T]oggle [T]heme' })
   end,
 }
