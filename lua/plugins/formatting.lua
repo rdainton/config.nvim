ba700102fc863c442fb1a83ac22eeb4c3a694533
Ryan Dainton <ryandainton@live.com>
2025-12-01 15:24:38 +0000

    feat: biome formatter config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -15,6 +15,28 @@ return {
       typescript = { 'prettierd', 'prettier' },
       typescriptreact = { 'prettierd', 'prettier' },
       javascriptreact = { 'prettierd', 'prettier' },
+      -- javascript = { 'biome' },
+      -- typescript = { 'biome' },
+      -- typescriptreact = { 'biome' },
+      -- javascriptreact = { 'biome' },
+    },
+    formatters = {
+      ['biome-check'] = function()
+        local util = require 'conform.util'
+        return {
+          meta = {
+            url = 'https://biomejs.dev/reference/cli/#biome-check',
+            description = 'A toolchain for web projects, aimed to provide functionalities to maintain them.',
+          },
+          command = util.from_node_modules 'biome',
+          stdin = true,
+          args = { 'check', '--write', '--stdin-file-path', '$FILENAME' },
+          cwd = util.root_file {
+            'biome.json',
+            'biome.jsonc',
+          },
+        }
+      end,
     },
   },
 }
